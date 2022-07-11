f5d6e5c66e8efe5463bb2cf679d20b48741e4855
Joachim Westphal <elindorath@gmail.com>
2022-07-11 22:47:39 +0200

    silence command line tools install 2

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  xcode-select --install > /dev/null || echo "Command line tool are already installed"
+  xcode-select --install 2> /dev/null || echo "Command line tool are already installed"
   sudo xcodebuild -license
 fi
 
