b99f11759893158d1b77839ac076536cdf814646
Joachim Westphal <elindorath@gmail.com>
2022-07-11 22:44:10 +0200

    catch error in command line tools install

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  xcode-select --install
+  xcode-select --install || echo "Command line tool are already installed"
   sudo xcodebuild -license
 fi
 
